56a87dc99d47d68baceff9f02510e1e4d72b263e
Michael Schoonmaker <michael.r.schoonmaker@gmail.com>
2015-03-31 21:30:33 -0700

    Service: Respond with error when no handlers exist.

--- test/index.js
+++ test/index.js
@@ -59,3 +59,34 @@ test('get-set-get', function (t) {
       registry.destroy();
     });
 });
+
+test('bad command', function (t) {
+  var peer = forkPeer();
+
+  peer.on('exit', function () {
+    t.end();
+  });
+
+  var registry = services.createRegistry();
+
+  t.plan(1);
+
+  var timerId = setTimeout(function () {
+    t.fail('Timeout');
+
+    peer.disconnect();
+    registry.destroy();
+  }, 1000);
+
+  registry.request('test:invalid', {})
+    .then(function () {
+      t.fail('Expected to throw.');
+    }, function (err) {
+      t.equal(err.message, 'Not Found: test:invalid');
+
+      clearTimeout(timerId);
+
+      peer.disconnect();
+      registry.destroy();
+    });
+});
